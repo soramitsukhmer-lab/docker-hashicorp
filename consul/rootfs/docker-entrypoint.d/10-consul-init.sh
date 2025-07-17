31aa38fcf22fd621feb50599f413ba4ab0bcf7ca
Socheat Sok <socheatsok78@gmail.com>
2025-07-17 14:29:42 +0700

    Add consul/rootfs/docker-entrypoint.d/

---
+++ consul/rootfs/docker-entrypoint.d/10-consul-init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [[ -z "${CONSUL_INIT}" ]]; then
+    exit 0
+fi
+
+entrypoint_log() {
+    if [ -z "${CONSUL_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+entrypoint_log "$0: Waiting for consul-init..."
+while [ ! -f /consul/config/consul-init.hcl ]; do
+    sleep 1
+done
+entrypoint_log "$0: consul-init completed, found /consul/config/consul-init.hcl"
