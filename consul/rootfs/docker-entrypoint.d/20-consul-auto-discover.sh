a6ca34a650e7ec13713131b645608e67ca5da26b
Socheat Sok <socheatsok78@gmail.com>
2025-07-17 17:31:29 +0700

    Add consul/rootfs/docker-entrypoint.d/20-consul-auto-discover.sh

---
+++ consul/rootfs/docker-entrypoint.d/20-consul-auto-discover.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+if [[ "${CONSUL_INIT}" != "true" ]]; then
+    exit 0
+fi
+
+if [ -z "$CONSUL_CONFIG_DIR" ]; then
+  CONSUL_CONFIG_DIR=/consul/config
+fi
+CONSUL_AUTO_DISCOVER_CONFIG="${CONSUL_CONFIG_DIR}/consul-discover.hcl"
+
+entrypoint_log() {
+    if [ -z "${CONSUL_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+entrypoint_log "$0: Checking consul-auto-discover configuration..."
+
+if [ ! -f "${CONSUL_AUTO_DISCOVER_CONFIG}" ]; then
+    entrypoint_log "$0: consul-auto-discover configuration file not found, waiting for consul-auto-discover..."
+    while [ ! -f "${CONSUL_AUTO_DISCOVER_CONFIG}" ]; do
+        sleep 1
+    done
+fi
+
+entrypoint_log "$0: consul-auto-discover completed, found ${CONSUL_AUTO_DISCOVER_CONFIG}"
