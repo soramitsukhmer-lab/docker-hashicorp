da0fecccf9d7a8e6def03407c7cfe3dd40f24bb2
Socheat Sok <socheatsok78@gmail.com>
2025-07-17 14:54:37 +0700

    Update consul-init/rootfs/consul-init.sh

--- consul-init/rootfs/consul-init.sh
+++ consul-init/rootfs/consul-init.sh
@@ -5,25 +5,44 @@ CONSUL_INIT_CONFIG="/consul/config/consul-init.hcl"
 
 echo "$ME: Initializing Consul configuration..."
 
-if [ -z "${CONSUL_ADVERTISE_INTERFACE}" ]; then
-  echo "$ME: CONSUL_ADVERTISE_INTERFACE is not set, using default 'eth0'."
-  CONSUL_ADVERTISE_INTERFACE=eth0
-else
-  echo "$ME: Using CONSUL_ADVERTISE_INTERFACE=${CONSUL_ADVERTISE_INTERFACE}."
-fi
-export CONSUL_ADVERTISE_ADDR=`sockaddr eval 'GetInterfaceIP "'${CONSUL_ADVERTISE_INTERFACE}'"'`
+# You can set CONSUL_ADVERTISE_INTERFACE to the name of the interface you'd like to
+# advertise to and this will look up the IP and pass the proper -advertise= option along
+# to Consul.
+if [ -z "$CONSUL_ADVERTISE" ]; then
+  if [ -n "$CONSUL_ADVERTISE_INTERFACE" ]; then
+    CONSUL_ADVERTISE_ADDRESS=$(ip -o -4 addr list $CONSUL_ADVERTISE_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
+    if [ -z "$CONSUL_ADVERTISE_ADDRESS" ]; then
+      echo "Could not find IP for interface '$CONSUL_ADVERTISE_INTERFACE', exiting"
+      exit 1
+    fi
 
-if [[ -n "${CONSUL_ADVERTISE_WAN_INTERFACE}" ]]; then
-    echo "$ME: Using CONSUL_ADVERTISE_WAN_INTERFACE=${CONSUL_ADVERTISE_WAN_INTERFACE}."
-    export CONSUL_ADVERTISE_ADDR_WAN=`sockaddr eval 'GetInterfaceIP "'${CONSUL_ADVERTISE_WAN_INTERFACE}'"'`
+    CONSUL_ADVERTISE="advertise_addr = \"$CONSUL_ADVERTISE_ADDRESS\""
+    echo "==> Found address '$CONSUL_ADVERTISE_ADDRESS' for interface '$CONSUL_ADVERTISE_INTERFACE', setting bind option..."
+  fi
+fi
+
+# You can set CONSUL_ADVERTISE_WAN_INTERFACE to the name of the interface you'd like to
+# advertise wan to and this will look up the IP and pass the proper -advertise-wan= option along
+# to Consul.
+if [ -z "$CONSUL_ADVERTISE_WAN" ]; then
+  if [ -n "$CONSUL_ADVERTISE_WAN_INTERFACE" ]; then
+    CONSUL_ADVERTISE_WAN_ADDRESS=$(ip -o -4 addr list $CONSUL_ADVERTISE_WAN_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
+    if [ -z "$CONSUL_ADVERTISE_WAN_ADDRESS" ]; then
+      echo "Could not find IP for interface '$CONSUL_ADVERTISE_WAN_INTERFACE', exiting"
+      exit 1
+    fi
+
+    CONSUL_ADVERTISE_WAN="advertise_addr_wan = \"$CONSUL_ADVERTISE_WAN_ADDRESS\""
+    echo "==> Found address '$CONSUL_ADVERTISE_WAN_ADDRESS' for interface '$CONSUL_ADVERTISE_WAN_INTERFACE', setting bind option..."
+  fi
 fi
 
 echo "$ME: Writing Consul configuration file to ${CONSUL_INIT_CONFIG}"
 cat <<CONSUL > "${CONSUL_INIT_CONFIG}"
 bind_addr = "0.0.0.0"
 client_addr = "0.0.0.0"
-${CONSUL_ADVERTISE_ADDR:+"advertise_addr = \"${CONSUL_ADVERTISE_ADDR}\""}
-${CONSUL_ADVERTISE_ADDR_WAN:+"advertise_addr_wan = \"${CONSUL_ADVERTISE_ADDR_WAN}\""}
+${CONSUL_ADVERTISE}
+${CONSUL_ADVERTISE_WAN}
 CONSUL
 
 # Print the configuration file
