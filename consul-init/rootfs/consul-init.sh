186b00a7c94756337a4640880bdb9c34c7f7c142
Socheat Sok <socheatsok78@gmail.com>
2025-07-17 14:59:59 +0700

    Improve consul-init/rootfs/consul-init.sh

--- consul-init/rootfs/consul-init.sh
+++ consul-init/rootfs/consul-init.sh
@@ -8,7 +8,7 @@ echo "$ME: Initializing Consul configuration..."
 # You can set CONSUL_ADVERTISE_INTERFACE to the name of the interface you'd like to
 # advertise to and this will look up the IP and pass the proper -advertise= option along
 # to Consul.
-if [ -z "$CONSUL_ADVERTISE" ]; then
+if [ -z "$CONSUL_ADVERTISE_ADDRESS" ]; then
   if [ -n "$CONSUL_ADVERTISE_INTERFACE" ]; then
     CONSUL_ADVERTISE_ADDRESS=$(ip -o -4 addr list $CONSUL_ADVERTISE_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
     if [ -z "$CONSUL_ADVERTISE_ADDRESS" ]; then
@@ -16,15 +16,16 @@ if [ -z "$CONSUL_ADVERTISE" ]; then
       exit 1
     fi
 
-    CONSUL_ADVERTISE="advertise_addr = \"$CONSUL_ADVERTISE_ADDRESS\""
-    echo "==> Found address '$CONSUL_ADVERTISE_ADDRESS' for interface '$CONSUL_ADVERTISE_INTERFACE', setting bind option..."
+    echo "==> Found address '$CONSUL_ADVERTISE_ADDRESS' for interface '$CONSUL_ADVERTISE_INTERFACE', setting advertise option..."
   fi
+else
+  echo "==> Using provided '$CONSUL_ADVERTISE_ADDRESS' for CONSUL_ADVERTISE_ADDRESS, setting advertise option..."
 fi
 
 # You can set CONSUL_ADVERTISE_WAN_INTERFACE to the name of the interface you'd like to
 # advertise wan to and this will look up the IP and pass the proper -advertise-wan= option along
 # to Consul.
-if [ -z "$CONSUL_ADVERTISE_WAN" ]; then
+if [ -z "$CONSUL_ADVERTISE_WAN_ADDRESS" ]; then
   if [ -n "$CONSUL_ADVERTISE_WAN_INTERFACE" ]; then
     CONSUL_ADVERTISE_WAN_ADDRESS=$(ip -o -4 addr list $CONSUL_ADVERTISE_WAN_INTERFACE | head -n1 | awk '{print $4}' | cut -d/ -f1)
     if [ -z "$CONSUL_ADVERTISE_WAN_ADDRESS" ]; then
@@ -32,17 +33,18 @@ if [ -z "$CONSUL_ADVERTISE_WAN" ]; then
       exit 1
     fi
 
-    CONSUL_ADVERTISE_WAN="advertise_addr_wan = \"$CONSUL_ADVERTISE_WAN_ADDRESS\""
-    echo "==> Found address '$CONSUL_ADVERTISE_WAN_ADDRESS' for interface '$CONSUL_ADVERTISE_WAN_INTERFACE', setting bind option..."
+    echo "==> Found address '$CONSUL_ADVERTISE_WAN_ADDRESS' for interface '$CONSUL_ADVERTISE_WAN_INTERFACE', setting advertise wan option..."
   fi
+else
+  echo "==> Using provided '$CONSUL_ADVERTISE_WAN_ADDRESS' for CONSUL_ADVERTISE_WAN_ADDRESS, , setting advertise wan option..."
 fi
 
 echo "$ME: Writing Consul configuration file to ${CONSUL_INIT_CONFIG}"
 cat <<CONSUL > "${CONSUL_INIT_CONFIG}"
 bind_addr = "0.0.0.0"
 client_addr = "0.0.0.0"
-${CONSUL_ADVERTISE}
-${CONSUL_ADVERTISE_WAN}
+${CONSUL_ADVERTISE_ADDRESS:+"advertise_addr = \"${CONSUL_ADVERTISE_ADDRESS}\""}
+${CONSUL_ADVERTISE_WAN_ADDRESS:+"advertise_wan_addr = \"${CONSUL_ADVERTISE_WAN_ADDRESS}\""}
 CONSUL
 
 # Print the configuration file
